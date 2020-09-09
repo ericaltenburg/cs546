27ef639909cc458757e39ca7b953f62608ec2d1c
Eric Altenburg <ealtenbu@stevens.edu>
2020-09-08 23:12:33 -0400

    Added shell for lab1

---
+++ labs/lab1/lab1.js
@@ -0,0 +1,30 @@
+// Author: 			Eric Altenburg
+// Date: 			11 September 2020
+// Description:  	Lab 1
+// Pledge: 			I pledge my honor that I have abided by the Stevens Honor System.
+
+const questionOne = function questionOne(arr) {
+    // Implement question 1 here
+}
+
+const questionTwo = function questionTwo(arr) { 
+    // Implement question 2 here
+}
+
+const questionThree = function questionThree(text) {
+    // Implement question 3 here
+}
+
+const questionFour = function questionFour(num1, num2,num3) {
+    // Implement question 4 here
+}
+
+module.exports = {
+    firstName: "YOUR FIRST NAME", 
+    lastName: "YOUR LAST NAME", 
+    studentId: "YOUR STUDENT ID",
+    questionOne,
+    questionTwo,
+    questionThree,
+    questionFour
+};
