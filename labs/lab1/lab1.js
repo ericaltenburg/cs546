5e57205feea341d5de1302c018ca44c1f3fd8eb9
Eric Altenburg <ealtenbu@stevens.edu>
2020-09-10 10:00:21 -0400

    Finished lab 1, added submission zip

--- labs/lab1/lab1.js
+++ labs/lab1/lab1.js
@@ -1,28 +1,106 @@
-// Author: 			Eric Altenburg
-// Date: 			11 September 2020
-// Description:  	Lab 1
-// Pledge: 			I pledge my honor that I have abided by the Stevens Honor System.
+/*
+ * Author: 			Eric Altenburg
+ * Date: 			11 September 2020
+ * Description:  	Lab 1
+ * Pledge: 			I pledge my honor that I have abided by the Stevens Honor System.
+ */
 
 const questionOne = function questionOne(arr) {
     // Implement question 1 here
+    let primes = {};
+    if (arr === undefined || arr.length === 0) { // Error handling
+    	return primes;
+    }
+
+    for (let i = 0; i < arr.length; i++) { // For each number in arr
+    	let num = arr[i];
+    	let prime = true;
+
+    	if (num == 0 || num == 1) { // 0 and 1 are not prime
+    		prime = false;
+    	} else {
+	    	for (let j = 2; j <= num / 2; j++) { // go up to half the number and do mods
+	    		if (num % j == 0) {
+	    			prime = false;
+	    		}
+	    	}    		
+    	}
+
+    	primes[num] = prime ? true : false;
+    }
+
+    return primes;
 }
 
 const questionTwo = function questionTwo(arr) { 
     // Implement question 2 here
+    if (arr === undefined || arr.length === 0) { // Error handling
+    	return 0;
+    }
+    let sum = 0;
+    const summation = (num) => sum += Math.pow(num, 2);
+    arr.forEach(summation);
+
+    return Math.sqrt(Math.pow(sum, 6));
 }
 
 const questionThree = function questionThree(text) {
     // Implement question 3 here
+    let qualities = {consonants: 0,  vowels: 0,  numbers: 0, spaces: 0,  punctuation: 0, specialCharacters: 0};
+    if (text === undefined || text.length === 0) { // Error handling
+    	return qualities;
+    }
+    // To limit the ascii numbers used for cap and low
+    text = text.toLowerCase();
+
+    for (let i = 0; i < text.length; i++) { // Grab ascii val for each char in string
+    	let asciiVal = text.charCodeAt(i);
+
+    	if (asciiVal > 96 && asciiVal < 123) { // a lowercase letter
+    		if (asciiVal === 97 || asciiVal === 101 || asciiVal === 105 || asciiVal === 111 || asciiVal === 117) { // a e i o u
+    			qualities.vowels += 1;
+    		} else { // rest
+    			qualities.consonants += 1;
+    		}
+    	} else if (asciiVal > 47 && asciiVal < 58) { // numbers
+    		qualities.numbers += 1;
+    	} else if (asciiVal === 32) { // space
+    		qualities.spaces += 1;
+    	} else if (asciiVal === 46 || asciiVal === 63 || asciiVal === 34 || asciiVal === 39 || asciiVal === 44 || asciiVal === 45 || asciiVal === 150 || asciiVal === 151 || asciiVal === 33 || asciiVal === 58 || asciiVal === 59 || asciiVal === 40 || asciiVal === 41 || asciiVal === 91 || asciiVal === 93 || asciiVal === 47) { // punctuation
+    		qualities.punctuation += 1;
+    	} else { // specialCharacter
+    		qualities.specialCharacters += 1;
+    	}
+    }
+
+    return qualities;
 }
 
 const questionFour = function questionFour(num1, num2,num3) {
     // Implement question 4 here
+    if (num1 === undefined || num2 === undefined || num3 === undefined || num1 === undefined || num1 < 0 || num2 < 0 || num3 < 0) { // Error handling
+		return -1;
+	}
+
+	// Equation from calculator soup :)
+	let PV = num1;
+	let i = (num2 / 100)/12;
+	let n = num3 * 12;
+	let iPrime = Math.pow((1+i), n);
+	let ans;
+	if (i === 0) {
+		ans = PV/n;
+	} else {
+		ans = (PV * i * iPrime)/(iPrime - 1);
+	}
+
+	return parseFloat(ans.toFixed(2), 10);
 }
 
 module.exports = {
-    firstName: "YOUR FIRST NAME", 
-    lastName: "YOUR LAST NAME", 
-    studentId: "YOUR STUDENT ID",
+    firstName: "Eric", 
+    lastName: "Altenburg", 
+    studentId: "10443481",
     questionOne,
     questionTwo,
     questionThree,
--- labs/lab1/lab1.test.js
+++ labs/lab1/lab1.test.js
@@ -1,18 +1,56 @@
-// Author: 			Eric Altenburg
-// Date: 			11 September 2020
-// Description:  	Lab 1
-// Pledge: 			I pledge my honor that I have abided by the Stevens Honor System.
+/*
+ * Author: 			Eric Altenburg
+ * Date: 			11 September 2020
+ * Description:  	Lab 1
+ * Pledge: 			I pledge my honor that I have abided by the Stevens Honor System.
+ */
 
 const lab1 = require("./lab1");
 
 console.log(lab1.questionOne([1, 2, 3])); 
 // should output {1: false, 2: true, 3: true}
+console.log(lab1.questionOne([5, 9, 13, 17, 20])); // {'5': true, '9':false, '13':true, '17':true, '20':false}
+console.log(lab1.questionOne([0, 31, 90, 6217, 80883])); // {'0':false, '31':true, '90':false, '6217':true, '80883':false}
+console.log(lab1.questionOne([1])); // {'1':false}
+console.log(lab1.questionOne()); // {}
+console.log(lab1.questionOne([])); // {}
+
+console.log();
 
 console.log(lab1.questionTwo([1,2,3])); 
 // should output 2744 
+console.log(lab1.questionTwo([1, 3, 5, 7, 9])); // 4492125
+console.log(lab1.questionTwo([1, 10])); // 1030301
+console.log(lab1.questionTwo([1])); // 1
+console.log(lab1.questionTwo([0])); // 0
+console.log(lab1.questionTwo([])); // 0
+
+console.log();
 
 console.log(lab1.questionThree("The quick brown fox jumps over the lazy dog.")); 
 // should output {consonants: 24, vowels: 11, numbers: 0, spaces: 8, punctuation: 1, specialCharacters: 0}
+console.log(lab1.questionThree("There are 3 ways to write: two, too, and to! </>"));
+// { consonants: 17, vowels: 13, numbers: 1, spaces: 10, punctuation: 5, specialCharacters: 2 }
+console.log(lab1.questionThree("Are we there yet?!"));
+// { consonants: 7, vowels: 6, numbers: 0, spaces: 3, punctuation: 2, specialCharacters: 0 }
+console.log(lab1.questionThree("<> !@ $42358*{}\\ is alien for \"I am stressed\""));
+// { consonants: 12, vowels: 9, numbers: 5, spaces: 8, punctuation: 3, specialCharacters: 8 }
+console.log(lab1.questionThree(""));
+// {consonants: 0,  vowels: 0,  numbers: 0, spaces: 0,  punctuation: 0, specialCharacters: 0}
+console.log(lab1.questionThree());
+// {consonants: 0,  vowels: 0,  numbers: 0, spaces: 0,  punctuation: 0, specialCharacters: 0};
+
+console.log();
 
 console.log(lab1.questionFour(25000, 3.11, 5)); 
 // should output: 450.44
+console.log(lab1.questionFour(30000, 10, 10)); 
+// 396.45
+console.log(lab1.questionFour(10, 1, 1)); 
+// 0.84
+console.log(lab1.questionFour(75000, 7.35, 20)); 
+// 597.33
+console.log(lab1.questionFour(48000, 3.1415926, 10)); 
+// 466.64
+console.log(lab1.questionFour(200, 0, 1));
+// 16.67
